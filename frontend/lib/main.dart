8a4cf154e538ee35202d912e690a7c65f88a1f7b
dalgajan <dalgajan@students.zhaw.ch>
2024-11-27 09:20:16 +0700

    hotfx

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -16,6 +16,7 @@ import 'widgets/bottom_bar.dart';
 import 'app_theme.dart';
 
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
 
   if (!kIsWeb) {
@@ -92,5 +93,13 @@ class MyApp extends StatelessWidget {
         return null;
       },
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
   }
 }
